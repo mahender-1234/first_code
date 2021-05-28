3b8fafe939073e6b8058cfdfc58e3c555fb23b30
mahender ramdas <mahenderramdas009@gmail.com>
2021-05-28 10:53:51 -0500

    adding changes

--- sample123/dummy.py
+++ sample123/dummy.py
@@ -1,3 +1,7 @@
 # this is a sample python file
 def sample():
     return 'return from sample function'
+def sample():
+    return 'return from sample function'
+def sample():
+    return 'return from sample function'
